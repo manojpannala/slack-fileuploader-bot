fe6b9a272556ebb97d74220d9e5de533dcd761ac
Manoj Reddy Pannala <manojpannala@outlook.com>
2023-03-01 16:03:50 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"Manjaro_WP.jpg"}
+
+	for _, file := range fileArr {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     file,
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL: %s\n", file.Name, file.URL)
+	}
+
+}
